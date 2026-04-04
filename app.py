2fb3166cb7f73a5d3cd289f7f7ff0e3f3d5e807f
Jamie Cheshire <cheshire@umn.edu>
2026-04-03 22:50:58 -0700

    Add ear training app

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask import Flask
 
 from routes.fretboard import bp as fretboard_bp
 from routes.progressions import bp as progressions_bp
+from routes.reference import bp as reference_bp
+from routes.ear_training import bp as ear_training_bp
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.register_blueprint(fretboard_bp)
     app.register_blueprint(progressions_bp)
+    app.register_blueprint(reference_bp)
+    app.register_blueprint(ear_training_bp)
     return app
 
 
