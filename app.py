3bf004d7be5fde2c70fa5436ac5e4b7b0f8f5a44
Jamie Cheshire <cheshire@umn.edu>
2026-04-03 22:39:10 -0700

    Add chord progressions page

--- app.py
+++ app.py
@@ -3,11 +3,13 @@
 from flask import Flask
 
 from routes.fretboard import bp as fretboard_bp
+from routes.progressions import bp as progressions_bp
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.register_blueprint(fretboard_bp)
+    app.register_blueprint(progressions_bp)
     return app
 
 
